90ba52a0facce23f9fdbebde2dc34702a842d7dc
wish911wish <wish911wish@gmail.com>
2019-12-27 08:27:58 +0900

    make event table

---
+++ src/app/Http/Controllers/EventsController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class EventsController extends Controller
+{
+    //
+
+    public function index()
+    {
+        return view('event.index');
+    }
+
+    public function add()
+    {
+        
+    }
+}
